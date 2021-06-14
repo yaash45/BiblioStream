ba3150d3eff689edacbba1465f6bfb1c1da2fa70
Yaash Jain <yaash.jain@alumni.ubc.ca>
2021-06-14 16:34:26 -0700

    Dynamically assign user id
    - Get existing row count from userinfo table and use that to assign id for next inserted value

--- src/bibliostream.py
+++ src/bibliostream.py
@@ -6,14 +6,21 @@ class BiblioStream:
         self.db = database
         self.db.connect_to_db()
 
+    def get_user_count(self) -> int:
+        result = self.db.query_db("SELECT Count(*) FROM UserInfo")
+        # need to extract count value from list of tuple returned
+        return int(result[0][0])
+
     def insert_user(self, name, email, phone) -> str:
+        user_count = self.get_user_count()
+
         user_email = self.db.insert_into_db(
             f"INSERT INTO UserContact(email, phone) \
             VALUES ('{email}', '{phone}') RETURNING email"
         )
         user_id = self.db.insert_into_db(
             f"INSERT INTO UserInfo(id, email, name) \
-            VALUES ({0}, '{email}', '{name}') RETURNING id"
+            VALUES ({user_count}, '{email}', '{name}') RETURNING id"
         )
 
         return f"Inserted user with id = {user_id} and email = {user_email}"
