16bd35a80e1cb7a270863b15ad29cfb3a10b046a
Yaash Jain <yaash.jain@alumni.ubc.ca>
2021-06-15 11:41:19 -0700

    Connect webpage form to Database

--- src/main.py
+++ src/main.py
@@ -6,6 +6,21 @@ from database import Database
 app = Flask(__name__)
 
 
+# Create instance of argument parser
+parser = argparse.ArgumentParser(description="Pass database config")
+
+# Define argument for database config file path
+parser.add_argument("DatabaseConfigPath", metavar="path", type=str)
+
+# Parse provided argument from CLI
+args = parser.parse_args()
+
+if args.DatabaseConfigPath != None:
+    # Create an instance of the Database
+    db = Database(args.DatabaseConfigPath)
+    bs = BiblioStream(db)
+
+
 @app.route("/")
 def index() -> str:
     return render_template("index.html")
@@ -17,28 +32,12 @@ def insert_user():
     user_email = request.form.get("email")
     user_phone = request.form.get("phone")
 
-    print(f"name = {user_name}, email = {user_email}, phone = {user_phone}")
+    result = bs.insert_user(user_name, user_email, user_phone)
 
-    return redirect("/")
-
-
-def main() -> None:
-    # Create instance of argument parser
-    parser = argparse.ArgumentParser(description="Pass database config")
-
-    # Define argument for database config file path
-    parser.add_argument("DatabaseConfigPath", metavar="path", type=str)
-
-    # Parse provided argument from CLI
-    args = parser.parse_args()
-
-    if args.DatabaseConfigPath != None:
-        # Create an instance of the Database
-        db = Database(args.DatabaseConfigPath)
-        bs = BiblioStream(db)
-        print(bs.insert_user("Yaash", "abc@ubc.ca", "1234567890"))
-        bs.end_session()
+    if result != None:
+        return redirect("/")
+    else:
+        return "Failed operation"
 
 
-if __name__ == "__main__":
-    app.run(host="localhost", port=8080, debug=True)
+app.run(host="localhost", port=8080, debug=True)
