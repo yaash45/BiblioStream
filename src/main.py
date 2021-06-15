e441c581cc702b83ce39629f80e255add47319a2
Yaash Jain <yaash.jain@alumni.ubc.ca>
2021-06-15 13:23:32 -0700

    Implement frontend for Project query
    - Select checkboxes to choose which columns to display

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import argparse
 from flask import Flask, render_template, request, redirect
+from flask.helpers import url_for
 from bibliostream import BiblioStream
 from database import Database
 
@@ -22,8 +23,8 @@ if args.DatabaseConfigPath != None:
 
 
 @app.route("/")
-def index() -> str:
-    return render_template("index.html")
+def index(project_result=None) -> str:
+    return render_template("index.html", project_result=project_result)
 
 
 @app.route("/insert_user", methods=["POST"])
@@ -63,6 +64,41 @@ def update_user_phone():
         return redirect("/")
     else:
         return "Failed operation"
+
+
+@app.route("/project_series", methods=["GET"])
+def project_series():
+
+    # If the get button is pressed, obtain data from database
+    if request.args.get("project_get", None):
+        seasons = None
+        episodes = None
+
+        if request.args.get("project_seasons", None):
+            seasons = request.args["project_seasons"]
+        if request.args.get("project_episodes", None):
+            episodes = request.args["project_episodes"]
+
+        # If both are None, query will throw error. So redirect to index.html
+        if seasons == None and episodes == None:
+            return redirect("/")
+
+        # Project based on selected checkboxes
+        if seasons != None and episodes != None:
+            project_result = f"[('showname', 'seasons', 'episodes')] : \
+                {bs.project_series(seasons=True, episodes=True)}"
+        elif seasons != None and episodes == None:
+            project_result = f"[('showname', 'seasons')] : {bs.project_series(seasons=True, episodes=False)}"
+        elif seasons == None and episodes != None:
+            project_result = f"[('showname', 'episodes')] : {bs.project_series(seasons=False, episodes=True)}"
+
+        return render_template("index.html", project_result=project_result)
+
+    # If reset button is pressed, simply redirect to home
+    elif request.args.get("project_reset", None):
+        return redirect("/")
+
+    return redirect("/")
 
 
 try:
