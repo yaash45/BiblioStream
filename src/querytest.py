4a6280834d4cf198ca9446134cfd424c815cabac
anamhira <hira.anam49@gmail.com>
2021-06-15 19:01:43 -0700

    Changed querytest to use unittest framework

--- src/querytest.py
+++ src/querytest.py
@@ -3,30 +3,33 @@ from bibliostream import BiblioStream
 from database import Database
 import unittest
 
-
+class querytest(unittest.TestCase):
 # Create instance of argument parser
-parser = argparse.ArgumentParser(description="Pass database config")
-
-    # Define argument for database config file path
-parser.add_argument("DatabaseConfigPath", metavar="path", type=str)
-
-    # Parse provided argument from CLI
-args = parser.parse_args()
-
-if args.DatabaseConfigPath != None:
-        # Create an instance of the Database
-    db = Database(args.DatabaseConfigPath)
-    bs = BiblioStream(db)
+    
+    def setUp(self) -> None:
         
+        parser = argparse.ArgumentParser(description="Pass database config")
+
+            # Define argument for database config file path
+        parser.add_argument("DatabaseConfigPath", metavar="path", type=str)
+
+            # Parse provided argument from CLI
+        args = parser.parse_args()
+        if args.DatabaseConfigPath != None:
+                    # Create an instance of the Database
+            self.db = Database(args.DatabaseConfigPath)
+            self.bs = BiblioStream(self.db)
+            
     
 
-def test_user_count():
-    bs.insert_user("Yaash", "abc@ubc.ca", "1234567890")
-    assert bs.get_user_count() == 2, "Should be 1"
+    def test_user_count(self):
         
+        #self.bs.insert_user("Anam", "abc@ubc.ca", "1234567890")
+        self.assertEqual(self.bs.get_user_count(),1, "Should be 1")
+            
 
 
 
 if __name__ == "__main__":
-    test_user_count()
-    bs.end_session()
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
+    
